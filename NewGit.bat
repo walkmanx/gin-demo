cb1b2fe746e417865d9ac971e0f7f930b57687b6
walkmanx <guochen@zhuoyuan-info.com>
2021-04-16 11:43:07 +0800

    commit

---
+++ NewGit.bat
@@ -0,0 +1,8 @@
+# echo
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/walkmanx/gin-demo.git
+git push -u origin main
+pause
---
+++ src/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusOK,
+			"message": "pong1",
+			"data":    "",
+		})
+	})
+
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
